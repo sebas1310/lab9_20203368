a720b2694ddc55a36c44fa53b3633f540763a846
sebas1310 <a20203369@pucp.edu.pe>
2023-06-20 20:31:09 -0500

    pokemon

---
+++ LAB920203368/src/main/java/com/example/lab920203368/Lab920203368Application.java
@@ -0,0 +1,13 @@
+package com.example.lab920203368;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920203368Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920203368Application.class, args);
+    }
+
+}
---
+++ LAB920203368/src/test/java/com/example/lab920203368/Lab920203368ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab920203368;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920203368ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
